01af6151e7832625fceae7bdd4adae3245b492d7
e0407750 <e0407750@nus.edu.sg>
2020-02-06 18:23:09 +0800

    This code works

---
+++ try1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+from sensor_msgs.msg import LaserScan
+
+def callback(msg):
+    # create numpy array
+    laser_range = np.array([msg.ranges])
+
+    # replace 0's with nan
+    lr2 = laser_range
+    lr2[lr2==0] = np.nan
+    # find index with minimum value
+    shortest = lr2[0][0]
+    printf("The shortest distance is %f",shortest)
+
+def scanner():
+            # initialize node
+    rospy.init_node('scanner', anonymous=True)
+
+    # set the update rate to 1 Hz
+    rate = rospy.Rate(1) # 1 Hz
+
+    # subscribe to LaserScan data
+    rospy.Subscriber('scan', LaserScan, callback)
+
+    # wait until it is time to run again
+    rate.sleep()
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    try:
+        scanner()
+    except  rospy.ROSInterruptException:
+        pass
